ab39ca655e29233d0ac5b6465878d7122c441568
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-02 16:28:03 +0900

    move contents which should be in Model from View

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,13 @@ class Article < ActiveRecord::Base
   has_many :stocks, dependent: :destroy
   acts_as_taggable
   acts_as_taggable_on :labels
+
+  def is_stocked?(user)
+    Stock.exists?(user_id: user.id, article_id: self.id)
+  end
+
+  def related_stock(user)
+    return self.stocks.find_by(user_id: user.id)
+  end
+
 end
