4c8b4e4f0cffb1d6d527af729a2682b61026bf42
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-05 15:13:00 +0900

    add relationship between gruop model and manager model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :group_users
   has_many :users, through: :group_users
+  has_one :group
 
   validates :name, presence: true
   validates :body, presence: true
