4355bb7662fa82987de562aaa6fd49b3f29dd61e
Kosuke Machida <matchbaske.3.27@gmail.com>
2017-02-21 16:21:22 +0900

    modified some places with rubocop

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::RMagick
 
   # 保存先はassets以下
@@ -12,14 +11,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # 画像のサイズの上限を200pxに設定
-  process :resize_to_limit => [200, 200]
+  process resize_to_limit: [200, 200]
 
   # 保存形式をJPGにする
-  process :convert => 'jpg'
+  process convert: 'jpg'
 
   # 拡張子をjpg, jpeg, pngに指定
   def extension_white_list
     %w(jpg jpeg png)
   end
-
 end
