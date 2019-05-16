535b8fb2e6b810128cbefcdefc743521641ea5ce
k0mach1 <matchbaske.3.27@gmail.com>
2019-05-16 11:41:38 +0900

    remove email confimation

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,7 +1,7 @@
 User.seed_once do |s|
   s.id = 1
   s.username = 'master'
-  s.email = 'master@finc.com'
+  s.email = 'master@machio.me'
   s.slack_name = '@IamMaster'
   s.password = 'IamMasterofFiNC'
 end
